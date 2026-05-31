d9bd96ff3ce7e66af69067a8fdf0eaf77b61a644
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 15:26:06 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class validatorCalidateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = validatorCalidateWorker()
+    print("Worker engine initialized.")
